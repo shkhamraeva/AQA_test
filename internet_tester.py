bec15200b6e5a0bc551b68d8bcc895451af69bcb
shohina <shkhamraeva2000@gmail.com>
2026-04-03 13:11:03 +0500

    Added 3-4 tasks

--- internet_tester.py
+++ internet_tester.py
@@ -13,13 +13,22 @@ with sync_playwright() as p:
 
     h1_text = page.locator("h1").inner_text()
     assert "the-internet" in h1_text.lower()
-
     print(f"✅ Сайт доступен. Заголовок: {h1_text}")
 
     current_url = navigate_to_example(page, "Form Authentication")
 
     assert "/login" in current_url, "Не тот URL"
-
     print(f"Перешли в: Form Authentication | URL: {current_url}")
+
+    page.locator("#username").fill("tomsmith")
+    page.locator("#password").fill("SuperSecretPassword!")
+
+    page.get_by_role("button", name="Login").click()
+    assert "/secure" in page.url, "Не тот URL"
+    print(f"✅ Успешный вход! URL: {page.url}")
+
+    page.get_by_role("link", name="Logout").click()
+    assert "/login" in page.url
+    print(f"✅ Успешный выход! URL: {page.url}")
 
     browser.close()
